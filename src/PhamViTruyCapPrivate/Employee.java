baa62144393bee537eaf9db21aca204249b7a92e
DangThiThuyTien <dangthuytien1711@gmail.com>
2024-08-15 21:42:11 +0700

    Update Class1508

---
+++ src/PhamViTruyCapPrivate/Employee.java
@@ -0,0 +1,10 @@
+package PhamViTruyCapPrivate;
+
+public class Employee {
+    private String name = "Thủy Tiên";
+    private int age = 30;
+    private void getInfo() {
+        System.out.println(name);
+        System.out.println(age);
+    }
+}
